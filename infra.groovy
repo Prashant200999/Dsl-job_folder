24c09b44e5957bd7476a2af11565db7cc9fdcc9b
Prashant200999 <160811688+Prashant200999@users.noreply.github.com>
2025-04-05 14:36:18 +0530

    Create infra.groovy

---
+++ infra.groovy
@@ -0,0 +1,159 @@
+folder('Job-DSL') {
+    displayName('Job-DSL')
+}
+
+folder('Job-DSL/Env') {
+    displayName('Env')
+}
+
+folder('Job-DSL/Env/Dev') {
+    displayName('Dev')
+}
+
+folder('Job-DSL/Env/Dev/CI') {
+    displayName('CI')
+}
+
+folder('Job-DSL/Env/Dev/CI/Ansible') {
+    displayName('Ansible')
+}
+
+folder('Job-DSL/Env/Dev/CI/Ansible/Role') {
+    displayName('Role')
+}
+
+folder('Job-DSL/Env/Dev/CI/Ansible/Role/Redis') {
+    displayName('Redis')
+}
+
+folder('Job-DSL/Env/Dev/CI/Ansible/Playbook') {
+    displayName('Playbook')
+}
+
+folder('Job-DSL/Env/Dev/CI/Ansible/Playbook/Redis') {
+    displayName('Redis')
+}
+
+folder('Job-DSL/Env/Dev/CD') {
+    displayName('CD')
+}
+
+folder('Job-DSL/Env/Dev/CD/Delivery') {
+    displayName('Delivery')
+}
+
+folder('Job-DSL/Env/Dev/CD/Delivery/Ansible') {
+    displayName('Delivery')
+}
+
+folder('Job-DSL/Env/Dev/CD/Delivery/Ansible/Role') {
+    displayName('Role')
+}
+
+folder('Job-DSL/Env/Dev/CD/Delivery/Ansible/Role/Redis') {
+    displayName('Redis')
+}
+
+folder('Job-DSL/Env/Dev/CD/Delivery/Ansible/Role/PostgreSQL') {
+    displayName('PostgreSQL')
+}
+
+folder('Job-DSL/Env/Dev/CD/Delivery/Ansible/Role/ScyllaDB') {
+    displayName('ScyllaDB')
+}
+
+folder('Job-DSL/Env/Dev/CD/Delivery/Ansible/Playbook') {
+    displayName('Playbook')
+}
+
+folder('Job-DSL/Env/Dev/CD/Delivery/Ansible/Playbook/Redis') {
+    displayName('Redis')
+}
+
+folder('Job-DSL/Env/Dev/CD/Delivery/Ansible/Playbook/PostgreSQL') {
+    displayName('PostgreSQL')
+}
+
+folder('Job-DSL/Env/Dev/CD/Delivery/Ansible/Playbook/ScyllaDB') {
+    displayName('ScyllaDB')
+}
+
+folder('Job-DSL/Env/Dev/CD/Deployment') {
+    displayName('Deployment')
+}
+
+folder('Job-DSL/Env/Dev/CD/Deployment/Ansible') {
+    displayName('Ansible')
+}
+
+folder('Job-DSL/Env/Dev/CD/Deployment/Ansible/Role') {
+    displayName('Role')
+}
+
+folder('Job-DSL/Env/Dev/CD/Deployment/Ansible/Role/Redis') {
+    displayName('Redis')
+}
+
+folder('Job-DSL/Env/Dev/CD/Deployment/Ansible/Role/PostgreSQL') {
+    displayName('PostgreSQL')
+}
+
+folder('Job-DSL/Env/Dev/CD/Deployment/Ansible/Role/ScyllaDB') {
+    displayName('ScyllaDB')
+}
+
+folder('Job-DSL/Env/Dev/CD/Deployment/Ansible/Playbook') {
+    displayName('Playbook')
+}
+
+folder('Job-DSL/Env/Dev/CD/Deployment/Ansible/Playbook/Redis') {
+    displayName('Redis')
+}
+
+folder('Job-DSL/Env/Dev/CD/Deployment/Ansible/Playbook/PostgreSQL') {
+    displayName('PostgreSQL Playbook Folder')
+}
+
+folder('Job-DSL/Env/Dev/CD/Deployment/Ansible/Playbook/ScyllaDB') {
+    displayName('ScyllaDB Playbook Folder')
+}
+
+folder('Job-DSL/Env/Dev/Infra') {
+    displayName('Infra Folder')
+}
+
+folder('Job-DSL/Env/Dev/Infra/Application') {
+    displayName('Application Folder')
+}
+
+folder('Job-DSL/Env/Dev/Infra/Application/Frontend') {
+    displayName('Frontend Folder')
+}
+
+folder('Job-DSL/Env/Dev/Infra/Application/Salary') {
+    displayName('Salary Folder')
+}
+
+folder('Job-DSL/Env/Dev/Infra/Application/Employee') {
+    displayName('Employee Folder')
+}
+
+folder('Job-DSL/Env/Dev/Infra/Application/Attendance') {
+    displayName('Attendance Folder')
+}
+
+folder('Job-DSL/Env/Dev/Infra/Database') {
+    displayName('Database Folder')
+}
+
+folder('Job-DSL/Env/Dev/Infra/Database/Redis') {
+    displayName('Redis Database Folder')
+}
+
+folder('Job-DSL/Env/Dev/Infra/Database/PostgreSQL') {
+    displayName('PostgreSQL Database Folder')
+}
+
+folder('Job-DSL/Env/Dev/Infra/Database/ScyllaDB') {
+    displayName('ScyllaDB Database Folder')
+}
