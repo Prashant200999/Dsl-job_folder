6bc9c31c05556f42e040e81edef74f39a6505e82
Prashant200999 <160811688+Prashant200999@users.noreply.github.com>
2025-04-05 18:20:27 +0530

    Update infra.groovy

--- infra.groovy
+++ infra.groovy
@@ -226,42 +226,6 @@ job('Job-DSL/Env/Dev/CD/Delivery/Ansible/Role/ScyllaDB/Job-DSL') {
 // }
 
 
-
-
-
-
-
-
-pipelineJob('Job-DSL/Env/Dev/Infra/Database/PostgreSQL') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/snaatak-Zero-Downtime-Crew/job-dsl.git')
-                        credentials('git-cred')
-                    }
-                    branch('Shashi-SCRUM-310')
-                }
-            }
-        }
-    }
-    steps {
-        // Use setScriptText for Groovy script
-        dsl {
-            external('Infra/Database/PostgreSQL/infra.groovy')
-            lookupStrategy('SEED_JOB')
-        }
-    }
-}
-
-
-
-
-
-
-
-
 job('Job-DSL/Env/Dev/Infra/Database/ScyllaDB') {
     displayName('Job-DSL')
     description('Job ScyllaDB Infra from GitHub repository')
